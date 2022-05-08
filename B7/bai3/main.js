a0a6cd86c6caea633dbeca651e17a2bd27ac6fa1
thanhien65 <hiennguyenthanh651@gmail.com>
2022-05-08 20:30:02 +0700

    Done

--- B7/bai3/main.js
+++ B7/bai3/main.js
@@ -1,13 +1,3 @@
-// let oneyear = document.createElement("h2");
-// oneyear.innerText = `So thang trong nam`
-// document.body.appendChild(oneyear)
-// for (let month = 1; month < 13; month++) {
-//     // console.log(month);
-//     // document.write("thang ", month )
-//     const pushcontent = document.createElement("p");
-//     pushcontent.innerText = `Thang ${month}`;
-//     document.body.appendChild(pushcontent);
-// }
 let getinput = parseInt(prompt("Enter a Value", "1"));
 if (isNaN(getinput)) {
     alert('It is not a number'); 
