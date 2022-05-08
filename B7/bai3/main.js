39cbb274bc4d0ffac95ff8f0ef7dfa878a382ed9
thanhien65 <hiennguyenthanh651@gmail.com>
2022-05-08 20:51:25 +0700

    Done

--- B7/bai3/main.js
+++ B7/bai3/main.js
@@ -3,13 +3,15 @@ if (isNaN(getinput)) {
     alert('It is not a number'); 
 } else if (getinput === 0) {
     alert('0 x n = 0 ');
-} else {
-   let createtittle = document.createElement("h2")
-   createtittle.innerText =`bang cuu chuong ${getinput}`
-   document.body.appendChild(createtittle)
+} else if (getinput > 9) {
+    alert('please enter a value 1 --> 9');
+} else{
+    let createtittle = document.createElement("h2")
+    createtittle.innerText =`bang cuu chuong ${getinput}`
+    document.body.appendChild(createtittle)
     for (let i = 1; i < 11; i++){
-        const result = document.createElement("p")
-        result.innerText = `${getinput} x ${i} = ${getinput * i}`
-        document.body.appendChild(result)
+         const result = document.createElement("p")
+         result.innerText = `${getinput} x ${i} = ${getinput * i}`
+         document.body.appendChild(result)
     }
 }
