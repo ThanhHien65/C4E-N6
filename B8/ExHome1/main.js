2c693436cb528f2de395e72d83fb9f4ce9a4435a
thanhien65 <hiennguyenthanh651@gmail.com>
2022-05-10 23:04:22 +0700

    Done

---
+++ B8/ExHome1/main.js
@@ -0,0 +1,49 @@
+// let Arithmetic = prompt("enter Arithmetic + - * : ","+")
+// let NumFirst = Number(prompt("enter a value first ","1"))
+// let NumSecond = Number(prompt("enter a value first ","2"))
+// if (Arithmetic == '+'){
+//     var result = NumFirst + NumSecond
+// }else if(Arithmetic == '-'){
+//     var result = NumFirst - NumSecond
+// }else if(Arithmetic == '*'){
+//     var result = NumFirst * NumSecond
+// }else if(Arithmetic == ':'){
+//     var result = NumFirst / NumSecond
+// }else{
+//     alert('please enter Arithmetic + - * : "')
+// }
+// console.log(`${NumFirst} ${Arithmetic} ${NumSecond} = ${result}`);
+
+// let Arithmetic = prompt("enter Arithmetic + - * : ","+")
+// let NumFirst = Number(prompt("enter a value first ","1"))
+// let NumSecond = Number(prompt("enter a value first ","2"))
+// switch (Arithmetic){
+//     case '+':
+//         var result = NumFirst + NumSecond
+//         break;
+//     case '-':
+//         Arithmetic == "-"
+//         var result = NumFirst - NumSecond
+//         break;
+//     case '*':
+//         Arithmetic == "*"
+//         var result = NumFirst * NumSeconddd
+//         break;
+//     case ':':
+//         Arithmetic == ":"
+//         var result = NumFirst / NumSecond
+//         break;
+//     default :
+//         alert('please enter Arithmetic + - * : "')
+//         // break;
+// }
+// console.log(`${NumFirst} ${Arithmetic} ${NumSecond} = ${result}`);
+// parseInt is for converting a non integer number to an int and parseFloat is for converting a non float (with out a decimal) to a float (with a decimal).
+
+// If your were to get input from a user and it comes in as a string you can use the parse method to convert it to a number that you can perform calculations on.
+// var a = '5.0';
+// a = "5.5"
+// console.log(parseInt(a));
+// console.log(parseFloat(a));
+// console.log(Number(a));
+
