bc499e9257b29d44a45d813f6d0e031ebaa6c7f9
thanhien65 <hiennguyenthanh651@gmail.com>
2022-05-10 23:23:05 +0700

    Done

---
+++ B8/ExHome2/main.js
@@ -0,0 +1,9 @@
+let n= prompt("enter a value");
+    
+// console.log(Math.ceil('2.3'));
+// console.log(Math.floor('2'));
+if (Math.ceil(n) != Math.floor(n)){
+    console.log(n +" khong phai so nguyen");
+}else{
+    console.log(n +" la so nguyen");
+}
