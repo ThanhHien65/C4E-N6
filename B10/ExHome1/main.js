38163067548d9e4f4de0caeccd3b89d0cfe2af53
thanhien65 <hiennguyenthanh651@gmail.com>
2022-05-13 00:55:09 +0700

    Done

--- B10/ExHome1/main.js
+++
@@ -1,6 +0,0 @@
-function checkAge(age){
-    return (Number(age) < 18 ? true : Boolean(confirm("did parents allow your?")=== false)
-}
-function checkAge(age){
-    return((Number(age) < 18) || Boolean(Number(age) > 18) === false) === true ? true : Boolean(confirm("did parents allow your?")=== false)
-}
