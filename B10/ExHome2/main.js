6779855f18ff33f790164a7981e0c9ab0b68058c
Orther <hien@hanbiro.vn>
2022-05-15 09:59:49 +0700

    Done

---
+++ B10/ExHome2/main.js
@@ -0,0 +1,12 @@
+let obj = {
+  a: 2,
+  b: 1,
+  c: 3,
+};
+console.log(Object.keys(obj).map((name) => name.toUpperCase()));
+console.log(Object.entries(obj).map(([keys, value]) => keys.toUpperCase()));
+a = [];
+Object.entries(obj).forEach((key, value) => {
+  a.push(key[0].toUpperCase());
+});
+console.log(a);
